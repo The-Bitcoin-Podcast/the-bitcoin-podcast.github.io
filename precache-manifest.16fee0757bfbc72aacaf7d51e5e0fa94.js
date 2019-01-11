5ac0e8edced9b64882f9bd108fc9aff9e569b62c
Corey Petty <corey@status.im>
2019-01-11 08:38:02 -0500

    upload initial

---
+++ precache-manifest.16fee0757bfbc72aacaf7d51e5e0fa94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0063eda177e0044a2f44",
+    "url": "/static/js/main.0063eda1.chunk.js"
+  },
+  {
+    "revision": "cd24c01e20edd3e263d0",
+    "url": "/static/js/1.cd24c01e.chunk.js"
+  },
+  {
+    "revision": "0063eda177e0044a2f44",
+    "url": "/static/css/main.824a06a7.chunk.css"
+  },
+  {
+    "revision": "daf4e4019a6008fea05fd2caf41bfa99",
+    "url": "/index.html"
+  }
+];
